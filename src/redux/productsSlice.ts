e5565ab607712a42510b1a921848d1d78a8bb0a3
michael.estor <michael.estor@serino.com>
2022-11-03 13:47:11 +0800

    :art: update the immutability of the redux state

--- src/redux/productsSlice.ts
+++ src/redux/productsSlice.ts
@@ -21,16 +21,18 @@ export const productsSlice = createSlice({
     setProducts: (state: any, action: PayloadAction<any>) => {
       const listOfProducts = [...action.payload];
 
-      const subTotalCounter = [...state.value.products]
+      const subTotalCounter = listOfProducts
         .reduce((total: number, object: any) => {
           return total + object.total;
         }, 0)
         .toFixed(2);
 
-      //This will set the list in state
-      state.value.products = listOfProducts;
-      //This will update the subtotal of all the item prices
-      state.value.subTotal = subTotalCounter;
+      //This will set the list in state and update the subtotal of all the item prices
+      state.value = {
+        ...state.value,
+        products: listOfProducts,
+        subTotal: subTotalCounter,
+      };
     },
     removeProduct: (state: any, action: PayloadAction<any>) => {
       // Create a deep copy of a state
@@ -44,36 +46,42 @@ export const productsSlice = createSlice({
         }, 0)
         .toFixed(2);
 
-      //This will remove an item in the list
-      state.value.products = listOfFinalProducts;
-      //This will update the subtotal of all of the remaining item prices
-      state.value.subTotal = subTotalCounter;
+      //This will remove an item in the list and update the subtotal of all of the remaining item prices
+      state.value = {
+        ...state.value,
+        products: listOfFinalProducts,
+        subTotal: subTotalCounter,
+      };
     },
     updateQuantity: (state: any, action: PayloadAction<any>) => {
       //Will update a specific items in the list
     },
     increaseQuantity: (state: any, action: PayloadAction<any>) => {
-      const updatedQuantity = [...state.value.products].map((product: any) => {
-        if (product.id === action.payload.id) {
-          return {
-            ...product,
-            qty: product.qty + 1,
-            total: product.price * (product.qty + 1),
-          };
+      const updatedQuantity: {}[] = [...state.value.products].map(
+        (product: any) => {
+          if (product.id === action.payload.id) {
+            return {
+              ...product,
+              qty: product.qty + 1,
+              total: product.price * (product.qty + 1),
+            };
+          }
+          return product;
         }
-        return product;
-      });
+      );
 
-      const subTotalCounter = [...state.value.products]
+      const subTotalCounter: number = [...state.value.products]
         .reduce((total: number, object: any) => {
           return total + object.total;
         }, 0)
         .toFixed(2);
 
-      // Will increase a speicific item quantity
-      state.value.products = updatedQuantity;
-      //This will update the subtotal of all of the remaining item prices
-      state.value.subTotal = subTotalCounter;
+      // Will increase a speicific item quantity and update the subtotal of all of the remaining item prices
+      state.value = {
+        ...state.value,
+        products: updatedQuantity,
+        subTotal: subTotalCounter,
+      };
     },
     decreaseQuantity: (state: any, action: PayloadAction<any>) => {
       const updatedQuantity = [...state.value.products].map((product: any) => {
@@ -93,10 +101,13 @@ export const productsSlice = createSlice({
         }, 0)
         .toFixed(2);
 
-      // Will increase a speicific item quantity
-      state.value.products = updatedQuantity;
-      //This will update the subtotal of all of the remaining item prices
-      state.value.subTotal = subTotalCounter;
+      // Will increase a speicific item quantity and update the subtotal of all of the remaining item prices
+      state.value = {
+        ...state.value,
+        products: updatedQuantity,
+        subTotal: subTotalCounter,
+      };
+    },
   },
 });
 
